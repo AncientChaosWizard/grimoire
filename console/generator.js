2f4c6e39d7b104916bdb43fe496a76304cbee9ca
lserban <laurentiu.serban@lastingsoftware.com>
2023-08-07 14:23:28 +0300

    first commit

---
+++ console/generator.js
@@ -0,0 +1,7 @@
+spell_types=["simple","double","exceptional","authored"]
+/**
+ * simple=1 word
+ * double=2 words
+ * exceptional=2 words separated by a particle
+ * authored=author's name followed by type
+ */
